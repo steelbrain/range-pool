f7413705de0a53929e984a3d5f2c5a8a759797ab
steelbrain <aneesiqbalbhatti@gmail.com>
2016-02-03 16:11:34 -0700

    :fire: Remove sb-event-kit from package

--- src/worker.js
+++ src/worker.js
@@ -3,30 +3,23 @@
 /* @flow */
 
 import invariant from 'assert'
-import {CompositeDisposable, Emitter} from 'sb-event-kit'
-import type {Disposable} from 'sb-event-kit'
 
 export class PoolWorker {
   startIndex: number;
   currentIndex: number;
   limitIndex: number;
-  subscriptions: CompositeDisposable;
-  emitter: Emitter;
 
   constructor(startIndex: number, limitIndex: number) {
     invariant(typeof startIndex === 'number', 'startIndex is not a number')
     invariant(typeof limitIndex === 'number', 'limitIndex is not a number')
+    invariant(startIndex !== Infinity, 'startIndex should not be inifinite')
     invariant(startIndex > -1, 'startIndex should be at least zero')
     invariant(limitIndex > 0, 'limitIndex should be greater than zero')
     invariant(limitIndex - startIndex > 0, 'startIndex and limitIndex difference should be more than zero')
 
-    this.subscriptions = new CompositeDisposable()
-    this.emitter = new Emitter()
     this.startIndex = startIndex
     this.currentIndex = this.startIndex
     this.limitIndex = limitIndex
-
-    this.subscriptions.add(this.emitter)
   }
   advance(steps: number) {
     const remaining = this.getRemaining()
@@ -44,12 +37,7 @@ export class PoolWorker {
   hasCompleted(): boolean {
     return this.getRemaining() === 0
   }
-  onDidDestroy(callback: Function): Disposable {
-    return this.emitter.on('did-destroy', callback)
-  }
   dispose() {
-    this.emitter.emit('did-destroy')
     this.limitIndex = this.currentIndex
-    this.subscriptions.dispose()
   }
 }
